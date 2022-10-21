d3e2e03142faee3b981dfb7072b7ecff41c752b1
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-10-21 23:49:18 +0200

    add js task 01

---
+++ js/task-01.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const items = document.querySelectorAll('.js-item');
+console.log('Number of categories:', items.length);
+
+const titls = document.querySelectorAll('.js-title');
+console.log(titls);
+for (const title of titls) {
+        console.log('Category:', title.textContent);
+    
+};
+
+
+
+
+/*const title = document.querySelector('.js-title');
+console.log('Category:', title.textContent);*/
+
+const lists = document.querySelectorAll('.js-list');
+console.log(lists)
+for (const list of lists) {
+    
+}
+
+//console.log('Elements:', lists.children.length);
+
+
+
