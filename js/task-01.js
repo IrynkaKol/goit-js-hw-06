d3d116053f7bc9877935e1d594f48b44a069b265
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-10-22 01:08:12 +0200

    task 01 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,28 +1,14 @@
 "use strict";
 
-const items = document.querySelectorAll('.js-item');
-console.log('Number of categories:', items.length);
 
-const titls = document.querySelectorAll('.js-title');
-console.log(titls);
-for (const title of titls) {
-        console.log('Category:', title.textContent);
-    
-};
+const categoriesList = document.querySelectorAll('.item');
+//console.log(categoriesList)
+console.log('Number of categories:', categoriesList.length);
 
-
-
-
-/*const title = document.querySelector('.js-title');
-console.log('Category:', title.textContent);*/
-
-const lists = document.querySelectorAll('.js-list');
-console.log(lists)
-for (const list of lists) {
-    
-}
-
-//console.log('Elements:', lists.children.length);
+categoriesList.forEach(item => {
+    console.log('Category:', item.firstElementChild.textContent);
+    console.log('Elements:', item.lastElementChild.children.length)
+});
 
 
 
